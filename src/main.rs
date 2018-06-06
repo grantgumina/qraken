93ad8091ccb89e956858d00e25d2fa5b8df76edb
Grant J. Gumina <grantgumina@gmail.com>
2018-06-05 20:48:15 -0700

    template

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,15 @@ fn main() {
 
     let matches = App::new("Kraken")
         .version("0.1.0")
-        .author()
-        .about()
-        .arg()
+        .author("Grant Gumina")
+        .about("Allows you to run the same command on multiple machines over ssh")
+        .arg(Arg::with_name("FIRST_ARG")
+            .required(true)
+            .takes_value(true)
+            .index(1)
+            .help("No clue right now"))
+        .get_matches();
 
-    println!("Hello, world!");
+    let argument = matches.value_of("FIRST_ARG").unwrap();
+    println!("{}", argument);
 }
