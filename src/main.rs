f6a209ba0cfafe1ffd90160bef8cd4a89c6f607f
Grant J. Gumina <grantgumina@gmail.com>
2018-06-05 20:43:24 -0700

    first

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+extern crate clap;
+use clap::{Arg, App};
+
+fn main() {
+
+    let matches = App::new("Kraken")
+        .version("0.1.0")
+        .author()
+        .about()
+        .arg()
+
+    println!("Hello, world!");
+}
