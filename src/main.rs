226ef611f5dfe5c8bf80b46417547514ddd03104
Grant Gumina <ggumina@dyn-010-020-232-068.corp.qumulo.com>
2018-06-12 14:18:03 -0700

    broken af

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
 extern crate clap;
 extern crate ssh2;
 
+use std::sync::Arc;
+
 use std::io::prelude::*;
-use clap::{Arg, App};
+use std::thread::spawn;
+use std::time::Duration;
+use clap::{Arg, App, ArgMatches};
 use std::net::TcpStream;
 use ssh2::Session;
 
-// fn extract_arguments(matches: &ArgMatches) -> (Vec<&str>, String, String, String) {
-//     let address_list: Vec<_> = matches.values_of("IP_ADDRESS_LIST").unwrap().collect();
-//     let username = matches.value_of("USERNAME").unwrap().to_string();
-//     let password = matches.value_of("PASSWORD").unwrap().to_string();
-//     let command = matches.value_of("COMMAND").unwrap().to_string();
-
-//     (address_list, username, password, command)
-// }
-
 fn create_session(address: &str, username: &str, password: &str) -> (TcpStream, Session) {
     let tcp = TcpStream::connect(address).unwrap();
+    
+    // mutable reference will be copied...
     let mut session = Session::new().unwrap();
     session.handshake(&tcp).unwrap();
-        
+    
     match session.userauth_password(username, password) {
         Ok(_r) => {
-            println!("Successfully Authenticated to {}", address);
             (tcp, session)
         },
         Err(error) => {
@@ -31,9 +27,10 @@ fn create_session(address: &str, username: &str, password: &str) -> (TcpStream, 
     }
 }
 
-fn run_command(session: Session, command: &str) -> String {
+fn run_command(stream: &TcpStream, session: &Session, command: &str) -> String {
     let mut channel = session.channel_session().unwrap();
     let mut s = String::new();
+    
     channel.exec(command).unwrap();
     channel.read_to_string(&mut s).unwrap();
     
@@ -42,7 +39,7 @@ fn run_command(session: Session, command: &str) -> String {
 
 fn main() {
 
-    let matches = App::new("Kraken")
+    let app = App::new("Kraken")
         .version("0.1.0")
         .author("Grant Gumina")
         .about("Allows you to run the same command on multiple machines over ssh")
@@ -66,25 +63,40 @@ fn main() {
             .short("c")
             .required(true)
             .takes_value(true)
-            .help("Command to be executed on all servers specified"))
-        .get_matches();
+            .help("Command to be executed on all servers specified"));
+    
+    let matches = app.get_matches();
 
     let address_list: Vec<_> = matches.values_of("IP_ADDRESS_LIST").unwrap().collect();
     let username = matches.value_of("USERNAME").unwrap().to_string();
     let password = matches.value_of("PASSWORD").unwrap().to_string();
     let command = matches.value_of("COMMAND").unwrap().to_string();
 
-    // let mut connections = Vec::new();
+    // let mut connections: Vec<(TcpStream, Session)> = Vec::new();
 
-    // Create connections to each supplied address
+    let mut guards = vec![];
+
     for address in address_list {
-        let (_stream, session) = create_session(&address, &username, &password);
-        // connections.push(session);
-        let result = run_command(session, &command);
-        println!("\nCommand Output:\n===============");
-        print!("{}", result);
+        
+        let c = command.clone();
+        let a = address.clone();
+        guards.push(spawn(move || {
+            println!("{}", a);
+        }));
+        
+        // let (stream, session) =  create_session(address, &username, &password);
+        // // let stream_session_pair = create_session(address, &username, &password);
+
+        // let result = run_command(&stream, &session, &command);
+
+        // println!("\nCommand Output:\n===============");
+        // print!("{}", result);
     }
 
-    // let command = matches.value_of("COMMAND").unwrap();
-    // println!("{}", command);
+    for g in guards {
+        let _ = g.join();
+    }
+
+    // Create connections to each supplied address
+
 }
