c85f48536c6daaa5671cd65107f8ad087ba7ab1a
Grant Gumina <ggumina@Grant-Qumulo-MacBook-Pro.local>
2018-06-13 21:56:22 -0700

    added an output file option. need to get out of nested match hell though

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate clap;
 extern crate ssh2;
 extern crate indicatif;
+extern crate chrono;
 
 use std::io::prelude::*;
+use std::fs::OpenOptions;
 use std::thread::spawn;
+use chrono::prelude::*;
 use clap::{Arg, App};
 use std::sync::Arc;
 use std::net::TcpStream;
@@ -51,7 +54,13 @@ fn main() {
             .short("u")
             .required(true)
             .takes_value(true)
-            .help("Password for nodes being accessed"))
+            .help("Username for nodes being accessed"))
+        .arg(Arg::with_name("OUTPUT")
+            .short("o")
+            .required(false)
+            .takes_value(true)
+            .default_value("xxNONExx")
+            .help("File where command results are dumped"))
         .arg(Arg::with_name("COMMAND")
             .short("c")
             .required(true)
@@ -59,28 +68,26 @@ fn main() {
             .help("Command to be executed on all servers specified"));
     
     let matches = app.get_matches();
-    
     let address_list: Vec<_> = matches.values_of("IP_ADDRESS_LIST").unwrap().collect();
-    let username = matches.value_of("USERNAME").unwrap().to_string();
-    let password = matches.value_of("PASSWORD").unwrap().to_string();
-    let command = matches.value_of("COMMAND").unwrap().to_string();
 
+    // Setup progress bars
     let mp = Arc::new(MultiProgress::new());
     let bar_style = ProgressStyle::default_bar().template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}").progress_chars("##-");
 
     let mut guards = vec![];
 
     for (_iteration, address) in address_list.iter().enumerate() {
-        let c = command.to_string();
-        let u = username.to_string();
-        let p = password.to_string();
+
+        let u = matches.value_of("USERNAME").unwrap().to_string();
+        let p = matches.value_of("PASSWORD").unwrap().to_string();
+        let c = matches.value_of("COMMAND").unwrap().to_string();
+        let of = matches.value_of("OUTPUT").unwrap().to_string();
         let a = address.to_string();
 
         let pb = mp.add(ProgressBar::new(3));
         pb.set_style(bar_style.clone());
 
         guards.push(spawn(move || {
-            // let result: Result<(TcpStream, Session), ssh2::Error> =  create_session(&a, &u, &p)?;
 
             match create_session(&a, &u, &p) {
                 Ok(result) => {
@@ -88,10 +95,28 @@ fn main() {
                     pb.set_message(&format!("{}: Session created", a));
                     pb.inc(1);
                     
-                    let _ = run_command(&session, &c);
-                    
-                    pb.set_message(&format!("{}: Command Executed", a));
-                    pb.inc(2);
+                    let command_result = run_command(&session, &c).to_string();
+
+                    if of != "xxNONExx" {
+                        pb.set_message(&format!("{}: Writing to specified output file", a));
+
+                        match OpenOptions::new().append(true).create(true).open(of) {
+                            Ok(output_file) => {
+                                let file_contents = &format!("{}\n{}:\n{}\n", Utc::now(), &a, command_result);
+
+                                let mut writer = std::io::BufWriter::new(&output_file);
+                                writer.write_all(file_contents.as_bytes()).unwrap();
+
+                                pb.inc(2);
+                            },
+                            Err(e) => {
+                                pb.set_message(&format!("{}: Command Executed - File write error {}", a, e))
+                            }
+                        }
+                    } else {
+                        pb.set_message(&format!("{}: Command Executed", a));
+                        pb.inc(2);
+                    }
                     
                 },
                 Err(error) => {
